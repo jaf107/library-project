d980d781fb26716f12020c3852acb63dc11459f6
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-29 15:53:57 +0600

    Update update-book.component.ts

--- src/app/update-book/update-book.component.ts
+++ src/app/update-book/update-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
 import { BookService } from '../book.service';
 
 @Component({
@@ -13,6 +14,10 @@ export class UpdateBookComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.givenBook);
+  }
+
+  onSubmit(){
+
   }
 
 
