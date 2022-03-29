8148a381c3bf929d934559a99fa1d2ffe82752a7
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-29 14:58:31 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'book', component:BookListComponent},
+  {path: 'updatebook', component:UpdateBookComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
