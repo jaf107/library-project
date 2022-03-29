f3dc2bbacbf0bdcc0021a7bce5a331ee3aaff8d9
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-03-29 14:59:42 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { BookListComponent } from './book-list/book-list.component';
 import { UpdateBookComponent } from './update-book/update-book.component';
 
 const routes: Routes = [
+  {path:'', component:BookListComponent},
   {path: 'book', component:BookListComponent},
   {path: 'updatebook', component:UpdateBookComponent}, 
 ];
